35671a4953a1785297b26ad936ff1eb6d6d43930
Tomasz.Matczak <tmatczak@gmail.com>
2019-06-01 21:57:41 +0200

    Inicjalizacja

---
+++ pygame/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Klasa przeznaczona do przechowywania wszystkich ustawień gry """
+
+    def __init__(self):
+        """ Inicjalizacja ustawień gry"""
+        # Ustawienia gry
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
